162660ef312dd37bdd25eccc1faef9be7fd95c86
Praduman Singh Chundawat <pschundawat4080@gmail.com>
2023-02-15 16:08:25 +0530

    Mongodb connection problem

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ require("dotenv").config();
 const app = express();
 
 mongoose
-  .connect(process.env.DATABASE)
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("DB connected"))
   .catch((err) => console.log("DB connection error => ", err));
 
